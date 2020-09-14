61fa9e97ae435951bf0c67d849b045f9ce833fd4
aspen <asmith184@west-mec.org>
2020-09-14 15:29:31 -0700

    day 1

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
 //step 1 select objects on screen & define globals//
 
+const holes = document.querySelectorAll('.hole');
+const scoreboard = document.querySelectorAll('.score');
+const moles = document.querySelectorAll('mole');
+const countdownBoard = document.querySelector('.countdown');
+const startButton = document.querySelector('.startButton');
+
+let lastHole;
+let timeUp = false;
+let score = 0;
+let countdown;
+
+function pickRandomHole(holes){
+    const randomHole = Math.floor(Math.random() * holes.length);
+    const hole = holes[randomHole];
+    if (hole === lastHole){
+        return(pickRandomHole(holes));
+    }
+    lastHole = hole;
+    return hole;
+}
+
 //step 2 add class 'up'//
 
+function popOut(){
+    const time = Math.random() * 1300 + 400;
+    const hole = pickRandomHole(holes);
+    hole.classList.add('up');
+    setTimeout(function(){
+        hole.classList.remove('up');
+        if (!timeUp) popOut();
+    }, time);
+}
+
 //step 3 dynamically change text content to countdown//
+
+function startGame(){
+    countdown = timeLimit/20;
+    scoreBoard.textContent = 0;
+    scoreBoard.style.display = 'block';
+    countdownBoard.textContent = countdown;
+    timeUp = false;
+    score = 0;
+    popOut();
+    setTimeout(function(){
+        timeUp = true;
+    }, timeLimit);
+    let startCountdown = setInterval;
+}
 
 //step 4 adding event listeners//
 
