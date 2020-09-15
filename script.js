b96cc5ad882f5a0add8d01f1548eab7745e1d1dd
aspen <asmith184@west-mec.org>
2020-09-15 15:26:37 -0700

    day 2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //step 1 select objects on screen & define globals//
 
 const holes = document.querySelectorAll('.hole');
-const scoreboard = document.querySelectorAll('.score');
+const scoreboard = document.querySelector('.score');
 const moles = document.querySelectorAll('mole');
 const countdownBoard = document.querySelector('.countdown');
 const startButton = document.querySelector('.startButton');
@@ -10,6 +10,7 @@ let lastHole;
 let timeUp = false;
 let score = 0;
 let countdown;
+let timeLimit = 20000;
 
 function pickRandomHole(holes){
     const randomHole = Math.floor(Math.random() * holes.length);
@@ -37,8 +38,8 @@ function popOut(){
 
 function startGame(){
     countdown = timeLimit/20;
-    scoreBoard.textContent = 0;
-    scoreBoard.style.display = 'block';
+    scoreboard.textContent = 0;
+    scoreboard.style.display = 'block';
     countdownBoard.textContent = countdown;
     timeUp = false;
     score = 0;
@@ -46,9 +47,32 @@ function startGame(){
     setTimeout(function(){
         timeUp = true;
     }, timeLimit);
-    let startCountdown = setInterval;
+    let startCountdown = setInterval(function(){
+        countdown -= 1;
+        countdownBoard.textContent = countdown;
+        if(countdown < 0){
+            countdown = 0;
+            clearInterval(startCountdown);
+            countdownBoard.textContent = 'Times up!! Thank you for protecting our planet! Thos is the way!';
+        }
+    },1000)
 }
 
 //step 4 adding event listeners//
 
+startButton.addEventListener('click', startGame);
+
+function whack(e){
+    score++;
+    this.style.backgroundImage = 'url("yoda2.png")';
+    this.style.pointerEvents = 'none';
+    setTimeout(() => {
+        this.style.backgroundImage = 'url("yoda1.png")';
+        this.style.pointerEvents = 'all';
+    },800)
+    scoreboard.textContent = score;
+}
+
 //step 5 keep track of score with forEach//
+
+moles.forEach(mole => mole.addEventListener('click', whack));
